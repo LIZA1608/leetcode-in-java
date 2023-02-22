0bc79458f9c3eb94cf8f19a3d249271b95bb5c1b
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-02-22 21:48:57 +0530

    Create 713. Subarray Product Less Than K.java

---
+++ array/713. Subarray Product Less Than K.java
@@ -0,0 +1,17 @@
+/*https://leetcode.com/problems/sliding-window-maximum/description/*/
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int maxSum=0;
+        maxSum=nums;
+        
+        for(int i=k;i<n;i++){
+
+        }
+    }
+}
