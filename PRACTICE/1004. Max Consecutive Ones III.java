0134a51002d5068a1995f19feee7d9425c181c68
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-02-25 00:32:52 +0530

    Create 1004. Max Consecutive Ones III.java

---
+++ PRACTICE/1004. Max Consecutive Ones III.java
@@ -0,0 +1,16 @@
+/*https://leetcode.com/problems/max-consecutive-ones-iii/description/*/
+
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int i=0,j=0;
+for(j=0;j<nums.length;j++){
+    if(nums[j]==0){
+        k--;
+    }
+    if(k<0 && nums[i++]==0){
+        k++;
+    }
+}
+return j-i;
+    }
+}
