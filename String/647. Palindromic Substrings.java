a1cae3ee5f5e007754cf9efcfba74dddf6c86427
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-03-06 14:52:54 +0530

    Update 647. Palindromic Substrings.java

--- String/647. Palindromic Substrings.java
+++ String/647. Palindromic Substrings.java
@@ -3,10 +3,11 @@
 class Solution {
     public int countSubstrings(String s) {
         int ans=0;
+        //axis orbit approch
         //odd length
         for(int i=0;i<s.length();i++){
             for(int j=0;i-j>=0 && i+j<s.length();j++){
-                if(s.charAt((int)(i-j))==s.charAt((int)(i+j))){
+                if(s.charAt(i-j)==s.charAt(i+j)){
                     ans++;
                 }
                 else{
@@ -15,16 +16,17 @@ class Solution {
             }
         }
         //even length
-        for(double i=0.5;i<s.length();i++){
-            for(double j=0.5 ;i-j>=0 && i+j<s.length();j++){
-                if(s.charAt((int)(i-j))==s.charAt((int)(i+j))){
-                    ans++;
-                }
-                else{
-                    break;
-                }
-            }
+for(int l=0,r=1;r<s.length();l++,r++){
+    for(int orbit=0;l-orbit>=0&&r+orbit<s.length();orbit++){
+        if(s.charAt(l-orbit)==s.charAt(r+orbit)){
+            ans++;
         }
-        return ans;
+        else{
+            break;
+        }
+    }
+}
+
+     return ans;
     }
 }
