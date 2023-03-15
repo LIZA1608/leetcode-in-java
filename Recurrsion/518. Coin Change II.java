e52d3f2a0dbc2d15093026e74cbccaef34fe100e
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-03-15 14:10:50 +0530

    Create 518. Coin Change II.java

---
+++ Recurrsion/518. Coin Change II.java
@@ -0,0 +1,23 @@
+
+/*when use recurrion give tle*/
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n=coins.length;
+        return chtdg(amount,coins,0);
+    }
+    public int chtdg(int amount,int []coins,int i){
+        if(amount==0){
+            return 1;
+        }
+        if(i>=coins.length){
+            return 0;
+        }
+        int inc=0;
+        int exc=0;
+        if(amount>=coins[i]){
+            inc=chtdg(amount-coins[i],coins,i);
+        }
+        exc=chtdg(amount,coins,i+1);
+        return inc+exc;
+    }
+}
