6d370209b9519a8140220394fbd58d3441091aa5
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-03-27 14:18:31 +0530

    Create 112. Path Sum.java

---
+++ BinaryTree/112. Path Sum.java
@@ -0,0 +1,15 @@
+/*https://leetcode.com/problems/path-sum/*/
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+     if(root==null){
+         return false;
+     }   
+          if(root.left==null && root.right==null){
+           return  targetSum==root.val;          
+            }
+          boolean x=hasPathSum(root.left,targetSum-root.val);
+          boolean y=hasPathSum(root.right,targetSum-root.val);
+          return (x||y);
+    }
+}
