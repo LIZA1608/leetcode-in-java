b2dae5d82f18306912fcf2dc42d27aec36a54e42
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-04-05 14:24:47 +0530

    Create 387. First Unique Character in a String.java

---
+++ 387. First Unique Character in a String.java
@@ -0,0 +1,25 @@
+
+
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer>list=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            // if(list.containsKey(c)){
+            //     list.put(c,list.get(c)+1);
+            // }
+            // else{
+            //     list.put(c,1);
+            // }
+            //shortcut->
+            list.put(c,list.getOrDefault(c,0)+1);
+        }
+        for(int i=0;i<s.length();i++){
+            if(list.get(s.charAt(i))==1){
+                        return i;
+            }
+
+        }
+        return -1;
+    }
+}
