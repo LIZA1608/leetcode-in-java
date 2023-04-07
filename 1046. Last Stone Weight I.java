122c59ed3047a13c24ae037581e25188fa851d8c
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-04-07 14:17:08 +0530

    Create 1046. Last Stone Weight I.java

---
+++ 1046. Last Stone Weight I.java
@@ -0,0 +1,23 @@
+
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n=stones.length;
+         PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+      for(int i=0;i<n;i++){
+          pq.add(stones[i]);
+      }  
+      while(pq.size()>1){
+          int a=pq.poll();
+          int b=pq.poll();
+          if(a==b){
+              continue;
+          }
+          pq.add(a-b);
+    }
+    if(pq.size()==1){
+           return pq.poll();
+    }
+    return 0;
+}
+}
