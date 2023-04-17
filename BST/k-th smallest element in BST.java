ed5e4f974539c0331572de792154f79e405275ee
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-04-17 14:24:39 +0530

    Create k-th smallest element in BST.java

---
+++ BST/k-th smallest element in BST.java
@@ -0,0 +1,22 @@
+class Solution {
+    int i=0;
+     int solve(Node root,int k){
+        if(root==null){
+            return -1;
+        }
+        int left=solve(root.left,k);
+        if(left!=-1){
+            return left;
+        }
+        i++;
+        if(i==k){
+            return root.data;
+        }
+        return solve(root.right,k);
+    }
+    int KthSmallestElement(Node root, int K) {
+       
+        int ans=solve(root,K);
+        return ans;
+    }
+}
