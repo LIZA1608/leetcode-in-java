bd05cb2640b620257dfa0ab505a0ecf1eaff59f0
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2023-05-08 18:57:49 +0530

    Create 0-1 Knapsack.cpp

---
+++ Assignment 5/0-1 Knapsack.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, s;
+    cin >> n >> s;
+
+    vector<int> size(n+1), value(n+1);
+    for (int i = 1; i <= n; i++) {
+        cin >> size[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        cin >> value[i];
+    }
+
+    vector<vector<int>> dp(n+1, vector<int>(s+1, 0));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= s; j++) {
+            if (j >= size[i]) {
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-size[i]] + value[i]);
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    cout << dp[n][s] << endl;
+
+    return 0;
+}
